a598f2ee940089d4a00a81977719584410823e93
AminEl93 <amin_11_iverson@hotmail.com>
2023-10-07 13:26:03 +0200

    Buscador de héroes con la función de autocompletar

---
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormControl } from '@angular/forms';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+
+@Component({
+    selector: 'app-search-page',
+    templateUrl: './search-page.component.html'
+})
+
+export class SearchPageComponent {
+    
+    public searchInput = new FormControl('');
+    public heroes: Hero[] = [];
+    public selectedHero?: Hero;
+  
+    constructor(private _heroesService: HeroesService, private router: Router) { }
+  
+    searchHero() {
+        const value: string = this.searchInput.value || '';    
+        this._heroesService.getSuggestions(value).subscribe(heroes => this.heroes = heroes);
+    }  
+  
+    onSelectedOption(event: MatAutocompleteSelectedEvent): void {
+        if (!event.option.value) {
+            this.selectedHero = undefined;
+            return;
+        }       
+        const hero: Hero = event.option.value;
+        this.searchInput.setValue(hero.superhero);
+        this.selectedHero = hero;
+        
+        this._heroesService.getHeroById(hero.id)
+            .subscribe( heroe => {
+                this.selectedHero = heroe;
+                this.router.navigate(['/heroes', this.selectedHero?.id]);
+            });
+    }
+}
